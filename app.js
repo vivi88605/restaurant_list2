d086224df6c52fefb2b86f0d57b96a8d02468a51
vivi88605 <VIVI88605@gmail.com>
2023-06-24 17:34:19 +0800

    use restaurant.json in index.handlebars and show.handlebars set routes

--- app.js
+++ app.js
@@ -16,11 +16,14 @@ app.use(express.static('public'))// app.use(static('public'))
 //routes setting
 app.get('/', (req, res) => {
   // res.send(`<h1>aloha</h1>`)
-  res.render('index')
+  res.render('index', { restaurant: restaurantList.results })
 })
 
 app.get('/restaurants/:id', (req, res) => {
-  res.render('show')
+  const clicked = restaurantList.results.find(function (item) {
+    return item.id.toString() === req.params.id
+  })
+  res.render('show', { restaurant: clicked })
 })
 
 app.listen(port, (req, res) => {
