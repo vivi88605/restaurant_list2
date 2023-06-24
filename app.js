075397a8f3f87b5eb3434c6435284372dc9f8b0f
vivi88605 <VIVI88605@gmail.com>
2023-06-24 17:11:00 +0800

    install express-handlebars add layout.handlebars & index.handlebars & show.handlebars add index.css & show.css set static files set routes

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+//require packages
 const express = require('express')
 const app = express()
 const port = 3000
+//require express-handlebars
+const handlebars = require('express-handlebars')
 
+//!!setting template engines
+app.engine('handlebars', handlebars({ defaultLayout: 'layout' }))
+app.set('view engine', 'handlebars')
+
+//import files
+const restaurantList = require('./restaurant.json')
+app.use(express.static('public'))// app.use(static('public'))
+
+//routes setting
 app.get('/', (req, res) => {
-  res.send(`<h1>aloha</h1>`)
+  // res.send(`<h1>aloha</h1>`)
+  res.render('index')
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  res.render('show')
 })
 
 app.listen(port, (req, res) => {
